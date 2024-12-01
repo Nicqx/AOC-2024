afb386cfd1a63e99c5da555ad7067344c6765b38
cinerin-team <cinerin@mail.com>
2024-12-01 22:37:02 +0100

    day1

---
+++ Days/Day1.py
@@ -0,0 +1,47 @@
+import re
+
+from Utilities.read_file_to_string_array import read_to_string_array
+
+
+class Day1:
+    arr = []
+
+
+
+    def __init__(self, file):
+        self.arr = read_to_string_array(file)
+
+
+    def task1(self):
+        left = []
+        right = []
+        sum = 0
+        for element in self.arr:
+            pattern = r'(\d+)\s+(\d+)'
+            match = re.search(pattern, element)
+            left.append(int(match.group(1)))
+            right.append(int(match.group(2)))
+        left.sort()
+        right.sort()
+
+        while left:
+            sum += abs(left[0] - right[0])
+            left = left[1:]
+            right = right[1:]
+
+        return str(sum)
+
+    def task2(self):
+        left = []
+        right = []
+        sum = 0
+        for element in self.arr:
+            pattern = r'(\d+)\s+(\d+)'
+            match = re.search(pattern, element)
+            left.append(int(match.group(1)))
+            right.append(int(match.group(2)))
+
+        for element in left:
+            sum += right.count(element) * element
+
+        return str(sum)

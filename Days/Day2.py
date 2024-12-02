de2e3115b72e0ef1d30a0e1dcede775b04424ffd
cinerin-team <cinerin@mail.com>
2024-12-02 22:47:23 +0100

    day2

--- Days/Day2.py
+++ Days/Day2.py
@@ -3,6 +3,51 @@ import copy
 from Utilities.read_file_to_string_array import read_to_string_array
 
 
+def convert_string_array_to_int_array(loc_line):
+    return [int(item) for item in loc_line.split(" ")]
+
+
+def check_distance(loc_arr):
+    i = 1
+    while i < len(loc_arr):
+        dist = abs(loc_arr[i - 1] - loc_arr[i])
+        if dist < 1 or dist > 3:
+            return False
+        i = i + 1
+    return True
+
+
+def check_if_monoton(loc_arr, is_increase):
+    i = 1
+    while i < len(loc_arr):
+        if is_increase:
+            if loc_arr[i - 1] <= loc_arr[i]:
+                return False
+        elif loc_arr[i - 1] >= loc_arr[i]:
+            return False
+        i = i + 1
+    return True
+
+
+def check_safety(loc_arr):
+    if check_distance(loc_arr) and (
+            check_if_monoton(loc_arr, True)
+            or check_if_monoton(loc_arr, False)):
+        return True
+    else:
+        return False
+
+
+def check_safety_w_correction(loc_arr):
+    for i in range(len(loc_arr)):
+        tmp_list = copy.deepcopy(loc_arr)
+        tmp_list.remove(loc_arr[i])
+
+        if check_safety(tmp_list):
+            return True
+    return False
+
+
 class Day2:
     arr = []
 
@@ -12,9 +57,9 @@ class Day2:
     def task1(self):
         summa = 0
         for one_line in self.arr:
-            local_list = self.convert_string_array_to_int_array(one_line)
+            local_list = convert_string_array_to_int_array(one_line)
 
-            if self.check_safety(local_list):
+            if check_safety(local_list):
                 summa = summa + 1
 
         return str(summa)
@@ -22,51 +67,11 @@ class Day2:
     def task2(self):
         summa = 0
         for one_line in self.arr:
-            local_list = self.convert_string_array_to_int_array(one_line)
+            local_list = convert_string_array_to_int_array(one_line)
 
-            if self.check_safety(local_list):
+            if check_safety(local_list):
                 summa = summa + 1
-            elif self.check_safety_w_correction(local_list):
+            elif check_safety_w_correction(local_list):
                 summa = summa + 1
 
         return str(summa)
-
-    def convert_string_array_to_int_array(self, loc_line):
-        return [int(item) for item in loc_line.split(" ")]
-
-    def check_safety(self, loc_arr):
-        if self.check_distance(loc_arr) and (
-                            self.check_monotonity(loc_arr, True)
-                            or self.check_monotonity(loc_arr, False)):
-            return True
-        else:
-            return False
-
-    def check_safety_w_correction(self, loc_arr):
-        for i in range(len(loc_arr)):
-            tmp_list = copy.deepcopy(loc_arr)
-            tmp_list.remove(loc_arr[i])
-
-            if self.check_safety(tmp_list):
-                return True
-        return False
-
-    def check_distance(self, loc_arr):
-        i = 1
-        while i < len(loc_arr):
-            dist = abs(loc_arr[i - 1] - loc_arr[i])
-            if dist < 1 or dist > 3:
-                return False
-            i = i + 1
-        return True
-
-    def check_monotonity(self, loc_arr, is_increase):
-        i = 1
-        while i < len(loc_arr):
-            if is_increase:
-                if loc_arr[i - 1] <= loc_arr[i]:
-                    return False
-            elif loc_arr[i - 1] >= loc_arr[i]:
-                return False
-            i = i + 1
-        return True

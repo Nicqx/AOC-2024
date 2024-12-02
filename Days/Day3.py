07c83428d22b420791976156a92657ea96c10ba4
cinerin-team <cinerin@mail.com>
2024-12-02 23:34:47 +0100

    day3

---
+++ Days/Day3.py
@@ -0,0 +1,19 @@
+from Utilities.read_file_to_string_array import read_to_string_array
+
+
+class Day3:
+    arr = []
+
+    def __init__(self, file):
+        self.arr = read_to_string_array(file)
+
+
+    def task1(self):
+        sum = 0
+
+        return str(sum)
+
+    def task2(self):
+        sum = 0
+
+        return str(sum)
---
+++ Tests/test_Day3.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from Days.Day3 import Day3
+
+
+class TestDay3(TestCase):
+    def test_task1(self):
+        expected_value = str(11)
+        actual_value = Day3('../Resources/Day3/test1').task1()
+        self.assertEqual(expected_value, actual_value)
+
+    # def test_task2(self):
+    #     expected_value = str(31)
+    #     actual_value = Day3('../Resources/Day3/test1').task2()
+    #     self.assertEqual(expected_value, actual_value)
+    #
+    # def test_orig(self):
+    #     expected_value = str(1222801)
+    #     actual_value = Day3('../Resources/Day3/input').task1()
+    #     self.assertEqual(expected_value, actual_value)
+    #
+    #     expected_value = str(22545250)
+    #     actual_value = Day3('../Resources/Day3/input').task2()
+    #     self.assertEqual(expected_value, actual_value)

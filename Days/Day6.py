88494178e2093adc9a024a79e8fa5e5b7d793ab1
cinerin-team <cinerin@mail.com>
2024-12-06 21:32:27 +0100

    day6

--- Days/Day6.py
+++ Days/Day6.py
@@ -1,20 +1,104 @@
-import re
+from lxml.objectify import xsiannotate
 
 from Utilities.read_file_to_string_array import read_to_string_array
 
 
 class Day6:
     arr = []
+    actual_poz = (0, 0,"u")
+    direction = "u"
+    visited = set()
+    is_out = False
 
     def __init__(self, file):
         self.arr = read_to_string_array(file)
 
     def task1(self):
-        summa = 0
+        self.visited.clear()
+        self.is_out = False
+        self.find_start()
+        self.visited.add((self.actual_poz, "u"))
+        while not self.is_out:
+            if self.check_next():
+                self.move_next()
+            else:
+                self.turn_left()
 
-        return str(summa)
+        return str(len(self.visited))
 
     def task2(self):
         summa = 0
 
         return str(summa)
+
+    def find_start(self):
+        for y in range(len(self.arr)):
+            for x in range(len(self.arr[y])):
+                if self.arr[y][x] == "^":
+                    self.actual_poz = (x, y)
+                    return
+
+    def turn_left(self):
+        match self.direction:
+            case "u":
+                self.direction = "r"
+            case "l":
+                self.direction = "u"
+            case "d":
+                self.direction = "l"
+            case "r":
+                self.direction = "d"
+
+    def move_next(self):
+        match self.direction:
+            case "u":
+                self.actual_poz = (self.actual_poz[0], self.actual_poz[1] - 1)
+                self.visited.add((self.actual_poz, self.direction))
+            case "l":
+                self.actual_poz = (self.actual_poz[0] - 1, self.actual_poz[1])
+                self.visited.add((self.actual_poz, self.direction))
+            case "d":
+                self.actual_poz = (self.actual_poz[0],self.actual_poz[1] + 1)
+                self.visited.add((self.actual_poz, self.direction))
+            case "r":
+                self.actual_poz = (self.actual_poz[0] + 1, self.actual_poz[1])
+                self.visited.add((self.actual_poz, self.direction))
+
+    def check_next(self):
+        match self.direction:
+            case "u":
+                if self.actual_poz[1] == 0:
+                    self.is_out = True
+                    return False
+                elif self.arr[self.actual_poz[1] - 1][self.actual_poz[0]] == "#":
+                    return False
+                else:
+                    return True
+            case "l":
+                if self.actual_poz[0] == 0:
+                    self.is_out = True
+                    return False
+                elif self.arr[self.actual_poz[1]][self.actual_poz[0] - 1] == "#":
+                    return False
+                else:
+                    return True
+            case "d":
+                if self.actual_poz[1] == len(self.arr) - 1:
+                    self.is_out = True
+                    return False
+                elif self.arr[self.actual_poz[1] + 1][self.actual_poz[0]] == "#":
+                    return False
+                else:
+                    return True
+            case "r":
+                if self.actual_poz[0] == len(self.arr[0]) - 1:
+                    self.is_out = True
+                    return False
+                elif self.arr[self.actual_poz[1]][self.actual_poz[0] + 1] == "#":
+                    return False
+                else:
+                    return True
+
+    def correct_set(self):
+        # filter out the same positions with different directions
+        pass
--- Tests/test_Day6.py
+++ Tests/test_Day6.py
@@ -5,20 +5,25 @@ from Days.Day6 import Day6
 
 class TestDay1(TestCase):
     def test_task1(self):
-        expected_value = str(11)
+        expected_value = str(41)
         actual_value = Day6('../Resources/Day6/test1').task1()
         self.assertEqual(expected_value, actual_value)
 
     # def test_task2(self):
-    #     expected_value = str(31)
+    #     expected_value = str(6)
     #     actual_value = Day6('../Resources/Day6/test1').task2()
     #     self.assertEqual(expected_value, actual_value)
-    #
+
+    # def test_task2_obstacle(self):
+    #     expected_value = str(31)
+    #     actual_value = Day6('../Resources/Day6/test2').task1()
+    #     self.assertEqual(expected_value, actual_value)
+
     # def test_orig(self):
-    #     expected_value = str(1222801)
+    #     expected_value = str(4647)
     #     actual_value = Day6('../Resources/Day6/input').task1()
     #     self.assertEqual(expected_value, actual_value)
-    #
+    # #
     #     expected_value = str(22545250)
     #     actual_value = Day6('../Resources/Day6/input').task2()
     #     self.assertEqual(expected_value, actual_value)
